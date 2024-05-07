c6b73cee31df8a3b0656ee69d2f4467e96b51f7a
Aleksei Briukhachev <abriukhachev@trask.cz>
2024-05-07 08:51:11 +0200

    Sidebar

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,15 +1,15 @@
-INSERT INTO todo_user (name, email)
-VALUES ('John Doe', 'John@email.com'),
-       ('Jane Doe', 'jane@email.com'),
-       ('John Smith', 'smith@email.com');
+INSERT INTO todo_user (id, name, email)
+VALUES (1,'John Doe', 'John@email.com'),
+       (2, 'Jane Doe', 'jane@email.com'),
+       (3, 'John Smith', 'smith@email.com');
 
-INSERT INTO activity (title, description, start_date, end_date, user_id)
-VALUES ('Activity 1', 'Description 1', '2021-01-01', '2021-01-02', 100009),
-       ('Activity 2', 'Description 2', '2021-01-03', '2021-01-04', 100009),
-       ('Activity 3', 'Description 3', '2021-01-05', '2021-01-06', 100009),
-       ('Activity 4', 'Description 4', '2021-01-07', '2021-01-08', 100009),
-       ('Activity 5', 'Description 5', '2021-01-09', '2021-01-10', 100009),
-       ('Activity 6', 'Description 6', '2021-01-11', '2021-01-12', 100009);
+INSERT INTO activity (id, title, description, start_date, end_date, user_id)
+VALUES (1,'Activity 1', 'Description 1', '01-01-2024', null, 1),
+       (2,'Activity 2', 'Description 2', '01-03-2024', null, 2),
+       (3,'Activity 3', 'Description 3', '01-05-2024', null, 3),
+       (4,'Activity 4', 'Description 4', '01-07-2024', null, 2),
+       (5,'Activity 5', 'Description 5', '01-09-2024', null, 3),
+       (6,'Activity 6', 'Description 6', '01-11-2024', null, 1);
 
 
 
